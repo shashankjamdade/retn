a2b80b05a199f42263914336e9f21f57d0c79b17
shashankjamdade <jshashank388@gmail.com>
2021-08-22 19:10:22 +0530

    Revised

--- lib/fcm/PushNotificationService.dart
+++ lib/fcm/PushNotificationService.dart
@@ -14,6 +14,7 @@ class PushNotificationService {
       _fcm.requestNotificationPermissions(IosNotificationSettings());
     }
 
+
     // If you want to test the push notification locally,
     // you need to get the token and input to the Firebase console
     // https://console.firebase.google.com/project/YOUR_PROJECT_ID/notification/compose
